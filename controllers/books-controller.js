29792e1a10a11f5c70079ee4b800eb9658ea42f5
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-05-23 15:01:02 +0700

    get one book by id function

--- controllers/books-controller.js
+++ controllers/books-controller.js
@@ -4,23 +4,43 @@ mongoose.connect('mongodb://localhost/library');
 var Book = require('../models/books')
 
 var findAll = (req, res) => {
-  Book.find({}, function (err, books) {
-  if (err) return res.send(err);
-  res.send(books);
-  });
+  Book.find({})
+  .then((books)=>{
+    res.send(books)
+  })
+  .catch((err)=>{res.send(err)})
 }
 
 var newBook = (req, res) => {
-  Book.create(req.body, function (err, books) {
-  if (err) return res.send(err);
-  res.send('New Book has been added');
-  });
+  Book.create(req.body)
+  .then(()=>{
+    res.send('New Book has been added')
+  })
+  .catch((err)=>{res.send(err)})
+}
+
+var deleteBook = (req, res) => {
+  let id = req.params.id
+  Book.deleteOne({_id: id})
+  .then(()=>{
+    res.send('Book has been deleted')
+  })
+  .catch((err)=>{res.send(err)})
+}
+
+var getOneBook = (req, res) => {
+  let id = req.params.id
+  Book.find({_id: id})
+  .then((book)=>{
+    res.send(book)
+  })
+  .catch((err)=>{res.send(err)})
 }
 
 module.exports = {
   findAll,
-  newBook
-  // getOneBook,
-  // deleteBook,
+  newBook,
+  deleteBook,
+  getOneBook
   // updateBook
 }
--- routes/books.js
+++ routes/books.js
@@ -7,13 +7,13 @@ router.get('/', bookController.findAll);
 // create new book
 router.post('/', bookController.newBook)
 
-// // delete book
-// router.delete('/:id', bookController.deleteBook)
-//
+// delete book
+router.delete('/:id', bookController.deleteBook)
+
 // // update book
 // router.put('/:id', bookController.updateBook)
 //
-// // get book by id
-// router.get('/:id', bookController.getOneBook)
+// get book by id
+router.get('/:id', bookController.getOneBook)
 
 module.exports = router;
